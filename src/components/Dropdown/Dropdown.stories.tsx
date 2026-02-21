740af379e8a86bb7880bd072229828e3f423a2bc
Sito <40501794+sito8943@users.noreply.github.com>
2026-02-21 22:13:34 +0100

    story for action drop down

--- src/components/Dropdown/Dropdown.stories.tsx
+++ src/components/Dropdown/Dropdown.stories.tsx
@@ -1,6 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEllipsisV,
+  faEye,
+  faPen,
+  faTrash,
+} from "@fortawesome/free-solid-svg-icons";
 import { Dropdown } from "./Dropdown";
+import { Actions } from "../Actions";
+import { IconButton } from "../Buttons";
 
 const meta = {
   title: "Components/Dropdown/Dropdown",
@@ -26,6 +35,45 @@ export const Basic: Story = {
         </button>
         <Dropdown open={open} onClose={() => setOpen(false)}>
           <div className="p-4">This is a dropdown content.</div>
+        </Dropdown>
+      </div>
+    );
+  },
+};
+
+export const WithTextActionsAndIconTrigger: Story = {
+  render: () => {
+    const [open, setOpen] = useState(false);
+
+    return (
+      <div className="p-10">
+        <IconButton icon={faEllipsisV} onClick={() => setOpen(true)} />
+        <Dropdown open={open} onClose={() => setOpen(false)}>
+          <Actions
+            showActionTexts
+            showTooltips={false}
+            className="flex-col"
+            actions={[
+              {
+                id: "view",
+                tooltip: "View",
+                icon: <FontAwesomeIcon icon={faEye} />,
+                onClick: () => alert("View"),
+              },
+              {
+                id: "edit",
+                tooltip: "Edit",
+                icon: <FontAwesomeIcon icon={faPen} />,
+                onClick: () => alert("Edit"),
+              },
+              {
+                id: "delete",
+                tooltip: "Delete",
+                icon: <FontAwesomeIcon icon={faTrash} />,
+                onClick: () => alert("Delete"),
+              },
+            ]}
+          />
         </Dropdown>
       </div>
     );
