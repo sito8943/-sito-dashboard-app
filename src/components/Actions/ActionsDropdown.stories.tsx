ba1d860f15d48e508ffc90e3600bfbc699b52eb9
Sito <40501794+sito8943@users.noreply.github.com>
2026-02-23 14:02:47 +0100

    fixed action dropdown story

--- src/components/Actions/ActionsDropdown.stories.tsx
+++ src/components/Actions/ActionsDropdown.stories.tsx
@@ -1,11 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import React from "react";
 import { ActionsDropdown } from "./ActionsDropdown";
 import { faPen, faTrash, faEye } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import type { BaseEntityDto } from "lib";
-
-type Entity = BaseEntityDto & { id: string; createdAt: Date; updatedAt: Date; deletedAt?: Date | null };
 
 const meta = {
   title: "Components/Actions/ActionsDropdown",
@@ -13,9 +9,24 @@ const meta = {
   tags: ["autodocs"],
   args: {
     actions: [
-      { id: "view", tooltip: "View", icon: <FontAwesomeIcon icon={faEye} />, onClick: () => alert("View") },
-      { id: "edit", tooltip: "Edit", icon: <FontAwesomeIcon icon={faPen} />, onClick: () => alert("Edit") },
-      { id: "delete", tooltip: "Delete", icon: <FontAwesomeIcon icon={faTrash} />, onClick: () => alert("Delete") },
+      {
+        id: "view",
+        tooltip: "View",
+        icon: <FontAwesomeIcon icon={faEye} />,
+        onClick: () => alert("View"),
+      },
+      {
+        id: "edit",
+        tooltip: "Edit",
+        icon: <FontAwesomeIcon icon={faPen} />,
+        onClick: () => alert("Edit"),
+      },
+      {
+        id: "delete",
+        tooltip: "Delete",
+        icon: <FontAwesomeIcon icon={faTrash} />,
+        onClick: () => alert("Delete"),
+      },
     ],
   },
 } satisfies Meta<typeof ActionsDropdown>;
