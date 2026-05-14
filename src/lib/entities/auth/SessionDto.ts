d3296eeaa9045985a9d0eadeae836276ef346c8a
Sito8943 <sito8943@gmail.com>
2026-05-14 23:49:16 +0200

    securing drawer and opening base client

--- src/lib/entities/auth/SessionDto.ts
+++ src/lib/entities/auth/SessionDto.ts
@@ -1,10 +1,10 @@
-export type SessionDto = {
+type SessionDto<TExtra extends object = Record<string, never>> = {
   id: number;
   username: string;
   email: string;
   token: string;
   refreshToken?: string | null;
   accessTokenExpiresAt?: string | null;
-};
+} & TExtra;
 
 export type SessionAccountDto = Partial<SessionDto>;
